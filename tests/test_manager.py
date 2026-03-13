4e682ce214b13b3e4c98342ba402350b1c871b60
Sam-bot-dev <bavesh2006kumar@gmail.com>
2026-03-14 04:20:16 +0530

    storing response
    finally the responses are getting stored and also checked into the spellings.

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -22,9 +22,10 @@ if __name__ == "__main__":
 
     council = CouncilManager(agents)
     result = council.convene("Design a secure login system")
-
+    list_of_response = [] # this is the list i think where i will store reaponces of ai.
     for agent, response in result.items():
-        print(f"{agent}: {response}")
-        
+        # print(f"{agent}: {response}") #commented to see if i am able to store the respoces
+        list_of_response.append(response)# finally this works , let's go boyy...
+    print(list_of_response)  
     
     
